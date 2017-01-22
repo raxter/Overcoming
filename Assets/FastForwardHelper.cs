cdc241f82bb3cace6300825a98d2a0b26bb088c2
Richard Baxter <richard.jonathan.baxter@gmail.com>
2017-01-22 02:09:05 +0100

    vinegerette and other useful progress sauce

---
+++ Assets/FastForwardHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastForwardHelper : MonoBehaviour
+{
+    
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.F5) && Input.GetKey(KeyCode.F1))
+        {
+            Time.timeScale = 10;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+	}
+}
