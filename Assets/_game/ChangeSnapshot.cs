0896f4e77962cb467a7e37a23592aebe588bef23
Richard Baxter <richard.jonathan.baxter@gmail.com>
2017-02-20 00:39:47 +0100

    Overcoming music management

---
+++ Assets/_game/ChangeSnapshot.cs
@@ -0,0 +1,55 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Math)]
+    [Tooltip("Adds a value to a Float Variable.")]
+    public class ChangeSnapshot : FsmStateAction
+    {
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The Float variable to add to.")]
+        public AudioMixerSnapshot audioMixerSnapshotVariable;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The Float variable to add to.")]
+        public FsmFloat delayVariable;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The Float variable to add to.")]
+        public FsmFloat floatVariable;
+
+        public override void Reset()
+        {
+            audioMixerSnapshotVariable = null;
+            floatVariable = null;
+        }
+
+        public override void OnEnter()
+        {
+            DoFloatAdd();
+        }
+
+        public override void OnUpdate()
+        {
+            DoFloatAdd();
+        }
+
+        void DoFloatAdd()
+        {
+            StartCoroutine(playDelayed(delayVariable.Value));
+        }
+
+        IEnumerator playDelayed(float time)
+        {
+            yield return new WaitForSeconds(time);
+            audioMixerSnapshotVariable.TransitionTo(floatVariable.Value);
+            yield return null;
+
+        }
+    }
+}
