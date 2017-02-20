a2d31fb07759845e17eab4af5d125c814efc1c2e
Richard Baxter <richard.jonathan.baxter@gmail.com>
2017-02-21 00:06:48 +0100

    Intro and instruction screen

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+	public string sceneName;
+	
+	public void StartGame()
+	{
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+	}
+}
